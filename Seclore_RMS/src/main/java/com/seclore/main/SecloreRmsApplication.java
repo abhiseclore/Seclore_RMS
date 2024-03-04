c2c3f91a306067bccdc0076a692bca15747dd556
abhiseclore <162094492+abhiseclore@users.noreply.github.com>
2024-03-04 12:43:29 +0530

    Main Project

---
+++ Seclore_RMS/src/main/java/com/seclore/main/SecloreRmsApplication.java
@@ -0,0 +1,13 @@
+package com.seclore.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecloreRmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecloreRmsApplication.class, args);
+	}
+
+}
