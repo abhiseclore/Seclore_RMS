280719c5cb007770ad6ae3eae148186f9c15b907
Ashish Dhane <81252055+ashishbdhane@users.noreply.github.com>
2024-03-04 12:38:27 +0530

    Update and rename Database initializer to DatabaseInitializer.sql

--- DatabaseInitializer.sql
+++ DatabaseInitializer.sql
@@ -3,7 +3,7 @@ Database initializer
 CREATE TABLE user_details(
 	user_id INT IDENTITY(1,1),
 	name VARCHAR(60) NOT NULL,
-	password VARCHAR(255),
+	password VARCHAR(255) ,
 	position VARCHAR(60) NOT NULL,
 	is_active bit,
 	CONSTRAINT user_pk PRIMARY KEY(user_id),
