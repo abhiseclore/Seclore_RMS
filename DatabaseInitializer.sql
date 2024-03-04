bc45cadccb77887e5a71e9a2865c18e6a7cd2b87
Atharva Gharbude <59164709+zeroth-infinity@users.noreply.github.com>
2024-03-04 14:16:27 +0530

    Update DatabaseInitializer.sql
    Anurag's edit

--- DatabaseInitializer.sql
+++ DatabaseInitializer.sql
@@ -9,3 +9,35 @@ CREATE TABLE user_details(
 	CONSTRAINT user_pk PRIMARY KEY(user_id),
 );
 
+CREATE TABLE slot_master(
+	slot_id INT IDENTITY,
+	start_time TIME NOT NULL,
+	end_time TIME NOT NULL,
+	date DATE NOT NULL,
+	CONSTRAINT slot_id_pk PRIMARY KEY(slot_id),
+);
+
+go
+DECLARE @startDate DATE = '2024-03-01';
+ 
+WHILE @startDate <= '2024-03-10'
+BEGIN
+	DECLARE @startTime Time = '00:30:00';
+	INSERT INTO slot_master(date,start_Time,end_Time) VALUES ( @startDate,'00:00:00','00:30:00');
+	
+	WHILE @startTime != '00:00:00'
+	BEGIN
+		DECLARE @endTime Time =@startTime;
+ 
+		 SET @endTime =  DATEADD(MINUTE, 30, @endTime)
+ 
+		 INSERT INTO slot_master(date,start_Time,end_Time)
+		 VALUES ( @startDate,@startTime,@endTime);
+ 
+		 SET @startTime = @endTime;
+	END
+	SET @startDate =  DATEADD(DAY, 1, @startDate);
+END
+go
+
+select * from slot_master
