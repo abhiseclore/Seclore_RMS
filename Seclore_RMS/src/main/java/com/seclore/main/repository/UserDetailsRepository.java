4207bbc60ce080fe04b8ee1ed4fbd2c732ff89a8
Admin <Admin@AtharvaG19252.seclore.com>
2024-03-04 21:46:53 +0530

    repository commit

--- Seclore_RMS/src/main/java/com/seclore/main/repository/UserDetailsRepository.java
+++ Seclore_RMS/src/main/java/com/seclore/main/repository/UserDetailsRepository.java
@@ -7,47 +7,61 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.seclore.main.domain.UserDetails;
+
 @Repository
 public class UserDetailsRepository implements UserDetailsRepositoryInterface {
 
 	@Autowired
-	private JdbcTemplate jdbcTemplate ;
-	private static String ADD_NEW_USER="insert into user_details(name,password, position, is_active) values(?,?,?,?)";
-	
+	private JdbcTemplate jdbcTemplate;
+	private static String ADD_NEW_USER = "insert into user_details(name,password, position, is_active) values(?,?,?,?)";
+	private static String UPDATE_USER_DETAILS = "UPDATE user_details set name =?,password=? , position=? , is_active=? where user_id=?";
+	private static String SELECT_SINGLE_USER = "select * from user_details where user_id = ?";
+	private static String UPDATE_PASSWORD = "UPDATE user_details set password=? where user_id=?";
+	private static String SELECT_ALL_USERS = "select * from user_details";
+
 	@Override
 	public boolean addNewUser(UserDetails user) {
-		Object[] args = {user.getName()};
+		Object[] args = { user.getName(), user.getPassword(), user.getPosition(), true };
+		int rows = jdbcTemplate.update(ADD_NEW_USER, args);
+		if (rows > 0)
+			return true;
 		return false;
 	}
 
 	@Override
 	public boolean updateUserDetails(UserDetails user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean userLogin(int userId, String password) {
-		// TODO Auto-generated method stub
+		Object[] args = { user.getName(), user.getPassword(), user.getPosition(), user.isActive(), user.getUserId() };
+		int rows = jdbcTemplate.update(UPDATE_USER_DETAILS, args);
+		if (rows > 0)
+			return true;
 		return false;
 	}
 
 	@Override
 	public UserDetails findUser(int userId) {
-		// TODO Auto-generated method stub
-		return null;
+		UserDetailsRowMapper userDetailsRowMapper = new UserDetailsRowMapper();
+		try {
+			UserDetails userDetails = jdbcTemplate.queryForObject(SELECT_SINGLE_USER, userDetailsRowMapper, userId);
+			return userDetails;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
 	public List<UserDetails> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		List< UserDetails> usersList = jdbcTemplate.query(SELECT_ALL_USERS, new UserDetailsRowMapper());
+		return usersList;
 	}
 
 	@Override
 	public boolean updatePassword(int userId, String password) {
-		// TODO Auto-generated method stub
-		return false;
+		Object[] args = {password, userId};
+		
+		int rows = jdbcTemplate.update(UPDATE_PASSWORD, args);
+		if (rows > 0)
+			return true;
+		return false;	
 	}
 
 }
